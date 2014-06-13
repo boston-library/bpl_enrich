10ec1de6bc5f0ebbb19b3912563cdbffe7958d04
Steven Anderson <thephdgaming@gmail.com>
2014-06-13 15:39:52 -0400

    Updated a few month unit tests

--- test/dates_test.rb
+++ test/dates_test.rb
@@ -19,10 +19,16 @@ class DatesTest < ActiveSupport::TestCase
     assert_equal nil, result[:date_range]
     assert_equal 'April 7.1983 (Easter)', result[:date_note]
 
-    result = BplEnrich::Dates.standardize('1983.7.April (Easter)')
+    result = BplEnrich::Dates.standardize('1983.April.7 (Easter)')
     assert_equal '1983-04-07', result[:single_date]
     assert_equal nil, result[:date_range]
-    assert_equal 'April 7.1983 (Easter)', result[:date_note]
+    assert_equal '1983.April.7 (Easter)', result[:date_note]
+
+    result = BplEnrich::Dates.standardize('between April 2014 and May 2014')
+    assert_equal nil, result[:single_date]
+    assert_equal '2014-04', result[:date_range][:start]
+    assert_equal '2014-05', result[:date_range][:end]
+    assert_equal nil, result[:date_note]
   end
 
 
